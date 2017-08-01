7f105339a23cb8f8f497218628f010423f1fd567
pengfeng <pengfeng91@163.com>
2017-08-01 23:42:29 +0800

    shell

--- shell/mongodbDump.sh
+++ shell/mongodbDump.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd /root/mongodb-linux-i686-3.2.0/bin
+cd /root/mongodb/mongodb-linux-i686-3.2.0/bin
 
-./mongodump -h 127.0.0.1:27017 -d carSchool -o /data/dump
+./mongodump -h 127.0.0.1:27017 -d orderSysterm -o /data/dump
 
 cd /data/dump
 
-tar czvf db`date +%Y%m%d`.tar ./carSchool
+tar czvf db`date +%Y%m%d`.tar ./orderSysterm
 
 cp -p /data/dump/db`date +%Y%m%d`.tar /root/www/orderSysterm/dist/backups
