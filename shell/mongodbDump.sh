365fcfbeacba5fa802a6d3840ae83d17b0f10671
pengfeng <pengfeng91@163.com>
2017-08-02 22:44:56 +0800

    mongodb

--- shell/mongodbDump.sh
+++ shell/mongodbDump.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "dump start"
+
 cd /root/mongodb/mongodb-linux-x86_64-3.2.0/bin/
 
 ./mongodump -h 127.0.0.1:27017 -d orderSysterm -o /data/backups
@@ -9,3 +11,5 @@ cd /data/backups
 tar czvf db`date +%Y%m%d`.tar ./orderSysterm
 
 cp -p /data/backups/db`date +%Y%m%d`.tar /root/www/orderSysterm/dist/backups
+
+echo "dump end"
