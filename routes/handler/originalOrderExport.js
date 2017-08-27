787ba7f9ad85780eecb10d528c0216ac91a8932e
pengfeng <pengfeng91@163.com>
2017-08-27 09:46:03 +0800

    bug

--- routes/handler/originalOrderExport.js
+++ routes/handler/originalOrderExport.js
@@ -201,7 +201,6 @@ ORIGINALEXPORT = {
     //   $gte: listFilterStartTime,
     //   $lte: listFilterEndTime
     // } //{"order_date":{$lt:50}}
-    console.log(queryStr);
     this.getOrderListOriginalFromDB(function(docs, count) {
       res.send({
         result: 'TRUE',
